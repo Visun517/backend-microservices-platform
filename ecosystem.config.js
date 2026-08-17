dd9a017ee64cc4e61abb8985b423fe4f70e291d4
Visun <visunpraboda999@gmail.com>
2026-08-17 14:23:14 +0530

    add PM2 for platform services

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps : [
+        {
+            name: "config-server",
+            script: "java",
+            args: ["-jar", "./config-server/target/Config-Server-1.0.0.jar"],
+            log_file: "./config-server/logs/config-server.log"
+        },
+        {
+            name: "service-registry",
+            script: "java",
+            args: ["-jar", "./service-registry/target/Service-Registry-1.0.0.jar"],
+            log_file: "./service-registry/logs/service-registry.log"
+        },
+        {
+            name: "api-gateway",
+            script: "java",
+            args: ["-jar", "./api-gateway/target/Api-Gateway-1.0.0.jar"],
+            log_file: "./api-gateway/logs/api-gateway.log"
+        }
+    ]
+};
